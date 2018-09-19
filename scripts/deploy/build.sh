3bccd9699ae5a103a2e147d17525e4eb14637cc6
Patrick Keroulas <patrick.keroulas@gmail.com>
2018-09-19 16:52:31 -0400

    deploy: refactor
    - use absolute path in scripts to execute from anywhere
    - abort if errors occur
    - source child script in parent to preserve env settings
    - install all dependencies from a single script
    - clean-up the release dir layout
    - adapt the server dockerfile

--- scripts/deploy/build.sh
+++ scripts/deploy/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-cd ../../
-mkdir -p build
-cd build
+SCRIPT_DIR="$(dirname $(readlink -f $0))"
+TOP_DIR="$SCRIPT_DIR/../.."
+BUILD_DIR="$TOP_DIR/build"
+
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
 
 echo
 echo "Compiling CPP Code..."
@@ -10,18 +13,8 @@ cmake .. -DCMAKE_BUILD_TYPE=Release -DUSE_PCH=OFF -DBUILD_APPS=ON
 make -j4
 echo "Compiling CPP Code... done"
 
-cd ../
-cd apps/
-
 echo
 echo "Compiling GUI..."
-cd gui/
+cd $TOP_DIR/apps/gui/
 npm install && npm run production
 echo "Compiling GUI... done"
-
-cd ../
-
-echo
-echo "Compiling List Web Server..."
-cd listwebserver/
-echo "Compiling List Web Server... done"
--- scripts/setup-dev-env.sh
+++ scripts/setup-dev-env.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-sudo apt-get install -y uuid-dev libpcap-dev
+apt install -y uuid-dev libpcap-dev
 
 conan config install https://github.com/bisect-pt/conan_config.git
+
+if which node > /dev/null
+then
+    echo "node is installed, skipping..."
+else
+    apt install -y curl
+    curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+    bash nodesource_setup.sh
+    apt install -y nodejs npm
+fi
 
 echo "Please install FFMPEG v2.8 or newer using your package manager or https://www.ffmpeg.org/download.html"
 echo "If you're going to run LIST via Docker, ignore the above message, as FFMPEG it's already installed on Docker image"
