aea8c6c9573d1201029e52436e6a9c932075b887
Patrick Keroulas <patrick.keroulas@gmail.com>
2018-10-29 12:59:14 -0400

    static gen: add ancillary types

--- apps/static_generator/serializer.h
+++ apps/static_generator/serializer.h
@@ -7,4 +7,5 @@ namespace ebu_list
     void write_available_options(const path& dir);
     void write_available_options_for_video(const path& dir);
     void write_available_options_for_audio(const path& dir);
+    void write_available_options_for_ancillary(const path& dir);
 }
