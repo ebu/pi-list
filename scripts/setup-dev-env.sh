e01af4af178b2530dea324fe4ae74a64bf3f4fde
Pedro Ferreira <pedro@bisect.pt>
2019-02-13 12:30:01 +0000

    Merged PR 47: Revert changes to Docker build process
    Revert changes to Docker build process

--- scripts/setup-dev-env.sh
+++ scripts/setup-dev-env.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#
-# This script installs a build/dev environment on a Debian host.
 
 # get cmake 3.9
 echo "deb http://ftp.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/stretch-backports.list
@@ -8,20 +6,16 @@ apt update
 apt -t stretch-backports install -y --no-install-recommends cmake
 
 # utilities
-apt install -y \
-    curl \
-    libpcap-dev \
-    libssl-dev \
-    python-pip \
-    rsync \
-    uuid-dev
-
-# Conan is a python package used to build CPP dependencies
+apt install -y curl rsync python-pip
 pip install conan
 
-# custom node version
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
+# node
+curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+bash nodesource_setup.sh
 apt install -y nodejs npm
+rm -f nodesource_setup.sh
+
+apt install -y uuid-dev libpcap-dev
 
 echo "Please install FFMPEG v2.8 or newer using your package manager or https://www.ffmpeg.org/download.html"
 echo "If you're going to run LIST via Docker, ignore the above message, as FFMPEG it's already installed on Docker image"
