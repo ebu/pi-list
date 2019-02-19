572a5e039ac6af90a7a3a5f2279061b9465f15b8
Pedro Ferreira <pedro@bisect.pt>
2019-02-19 18:17:20 +0000

    Merged PR 56: Remove first packet first frame option for Vrx calculation
    Remove first packet first frame option for Vrx calculation
    
    Related work items: #166

--- cpp/libs/st2110/lib/src/ebu/list/st2110/d21/vrx_calculator.cpp
+++ cpp/libs/st2110/lib/src/ebu/list/st2110/d21/vrx_calculator.cpp
@@ -18,8 +18,45 @@ vrx_calculator::vrx_calculator(int npackets,
     vrx_settings settings)
     : settings_(settings),
     tframe_(1 / video_info.rate),
-    constants_(calculate_vrx_constants(npackets, tframe_, settings.schedule, video_info.scan, video_info.raster))
+    constants_(calculate_vrx_constants(npackets, tframe_, settings.schedule, video_info.scan, video_info.raster)),
+	trs_ns_(to_double(constants_.trs))
 {
+}
+
+void vrx_calculator::on_frame_start(const fraction64& packet_time)
+{
+	current_n_ = calculate_n(packet_time, tframe_);
+
+	if(settings_.tvd == tvd_kind::ideal)
+	{
+		const auto base_frame_time = current_n_ * tframe_;
+		const auto ideal_tvd = base_frame_time + constants_.tr_offset;
+
+		if(settings_.troffset.has_value())
+		{
+			tvd_ = base_frame_time + fraction64(settings_.troffset.value().count(), 1'000'000'000);
+		}
+		else
+		{
+			tvd_ = ideal_tvd;
+		}
+	}
+	else
+	{
+		LIST_ASSERT(settings_.tvd == tvd_kind::first_packet_first_frame);
+
+		if(!first_tvd_)
+		{
+			first_tvd_ = packet_time;
+		}
+
+		tvd_ = first_tvd_.value() + tframe_ * frame_count_;
+	}
+
+	drained_prev_ = 0;
+	vrx_prev_ = 0;
+
+	++frame_count_;
 }
 
 packet_info vrx_calculator::on_packet(const clock::time_point& packet_timestamp, bool frame_start)
@@ -31,65 +68,18 @@ packet_info vrx_calculator::on_packet(const clock::time_point& packet_timestamp,
 
     if (frame_start)
     {
-        current_n_ = calculate_n(packet_time, tframe_);
-
-        const auto base_frame_time = current_n_ * tframe_;
-        const auto ideal_tvd = base_frame_time + constants_.tr_offset;
-
-        if (settings_.tvd == tvd_kind::ideal)
-        {
-            if (settings_.troffset.has_value())
-            {
-                tvd_ = base_frame_time + fraction64(settings_.troffset.value().count(), 1'000'000'000);
-            }
-            else
-            {
-                tvd_ = ideal_tvd;
-            }
-        }
-        else if (settings_.tvd == tvd_kind::first_packet_first_frame)
-        {
-            if (!first_tvd_)
-            {
-                first_tvd_ = packet_time;
-            }
-
-            tvd_ = first_tvd_.value() + tframe_ * frame_count_;
-        }
-        else
-        {
-            LIST_ASSERT(settings_.tvd == tvd_kind::first_packet_each_frame);
-            tvd_ = packet_time;
-        }
-
-        drained_prev_ = 0;
-        
-        // TODO: do not reset
-        vrx_prev_ = 0;
-
-        current.delta_to_ideal_tpr0 = tvd_ - packet_time;
-
-        ++frame_count_;
-    }
+		on_frame_start(packet_time);
+		current.delta_to_ideal_tpr0 = tvd_ - packet_time;
+	}
 
     // TODO: should we continue to drain packets after Tvd + Npackets * Trs?
     // TODO: should we disallow Vrx underflow?
 
-	const auto pt = to_double(packet_time);
-	const auto tvd_p = to_double(tvd_);
-	const auto trs_p = to_double(constants_.trs);
-	const auto drained = static_cast<int>((pt - tvd_p + trs_p) / trs_p);
+	const auto packet_delta_ns = to_double(packet_time - tvd_);
+	const auto drained = static_cast<int>((packet_delta_ns + trs_ns_) / trs_ns_);
 
-    //const auto drained = static_cast<int>(floor((packet_time - tvd_ + constants_.trs) / constants_.trs));
     current.vrx = vrx_prev_ + 1 - (drained - drained_prev_);
-
-	if(current.vrx > 1000)
-	{
-		logger()->info("Ping");
-	}
-
     vrx_prev_ = current.vrx;
-
     drained_prev_ = drained;
 
     return current;
