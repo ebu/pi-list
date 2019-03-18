f32cefe9e8f5a6a68b92ec35ccd4326078349cae
Pedro Ferreira <pedro@bisect.pt>
2019-03-18 13:45:27 +0000

    Merged PR 91: Fix deal with no values in RTP analysis
    Fix deal with no values in RTP analysis

--- apps/listwebserver/enums/analysis.js
+++ apps/listwebserver/enums/analysis.js
@@ -9,6 +9,7 @@ module.exports = {
         compliant: 'compliant',
     },
     errors: {
+        missing_information: 'missing_information',
         invalid_delta_rtp_ts_vs_nt: 'errors.invalid_delta_rtp_ts_vs_nt',
         cinst_above_maximum: 'errors.cinst_above_maximum',
         vrx_above_maximum: 'errors.vrx_above_maximum',
