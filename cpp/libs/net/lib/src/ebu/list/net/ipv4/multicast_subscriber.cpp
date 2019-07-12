7acb8c466c2d23834f71af38766c68f288ac346a
Pedro Ferreira <pedro@bisect.pt>
2019-07-12 10:36:46 +0000

    Merged PR 133: Add some utility functions
    Related work items: #279

--- cpp/libs/net/lib/src/ebu/list/net/ipv4/multicast_subscriber.cpp
+++ cpp/libs/net/lib/src/ebu/list/net/ipv4/multicast_subscriber.cpp
@@ -1,18 +1,9 @@
 #include "ebu/list/net/ipv4/multicast_subscriber.h"
+#include "ebu/list/net/ipv4/multicast.h"
 #include "ebu/list/core/io/logger.h"
 #include "ebu/list/core/idioms.h"
 
-#if defined(LIST_HAS_POSIX)
-#include <netinet/in.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/ioctl.h>
-#include <netinet/in.h>
-#include <net/if.h>
-#include <arpa/inet.h>
-#elif defined(LIST_HAS_WIN32)
-#include <ws2ipdef.h>
-#endif
 
 using namespace ebu_list::ipv4;
 using namespace ebu_list;
@@ -25,27 +16,8 @@ multicast_subscription::multicast_subscription(const ipv4::address listen_addres
 	: sd_(AF_INET, SOCK_DGRAM, 0)
 {
     LIST_ENFORCE(sd_.is_valid(), std::runtime_error, "Error creating socket");
-    
-    // Enable SO_REUSEADDR to allow multiple applications to receive copies of the multicast datagrams.
-    int reuse = 1;
-    const auto setsockopt_result = setsockopt(sd_.get_handle(), SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<const char*>(&reuse), sizeof(reuse));
-    LIST_ENFORCE(setsockopt_result >= 0, std::runtime_error, "Error setting SO_REUSEADDR");
-    
-    // Bind to the proper port number with the IP address specified as INADDR_ANY.
-    struct sockaddr_in localSockAddr;
-    memset(reinterpret_cast<char*>(&localSockAddr), 0, sizeof(localSockAddr));
-    localSockAddr.sin_family = AF_INET;
-    localSockAddr.sin_port = static_cast<uint16_t>(multicast_port);
-    localSockAddr.sin_addr.s_addr = INADDR_ANY;
-    const auto bind_result = bind(sd_.get_handle(), reinterpret_cast<struct sockaddr*>(&localSockAddr), sizeof(localSockAddr));
-    LIST_ENFORCE(bind_result >= 0, std::runtime_error, "Error binding datagram socket");
-    
-    struct ip_mreq group;
-    group.imr_multiaddr.s_addr = static_cast<uint32_t>(multicast_address);
-    group.imr_interface.s_addr = static_cast<uint32_t>(listen_address);
-    const auto add_mc_result = setsockopt(sd_.get_handle(), IPPROTO_IP, IP_ADD_MEMBERSHIP, reinterpret_cast<const char*>(&group), sizeof(group));
-    LIST_ENFORCE(add_mc_result >= 0, std::runtime_error, "Error adding multicast group");
-    logger()->info("Reporting to multicast group {}.\n", to_string(multicast_address));
+
+    join_multicast_group(sd_, listen_address, ipv4::endpoint {multicast_address, multicast_port});
 }
 
 //------------------------------------------------------------------------------
---
+++ scripts/run_all.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SRC=$DIR/..
+
+INSTALL=0
+RUN_ALL=0
+
+usage() { echo "$0 usage:" && grep " .)\ #" $0; exit 0; }
+[ $# -eq 0 ] && usage
+
+while getopts ":hir" arg; do
+    case $arg in
+        i) # Install dependencies
+            INSTALL=1
+        ;;
+        
+        r) # Run all
+            RUN_ALL=1
+        ;;
+        
+        h | *) # Display help.
+            usage
+            exit 0
+        ;;
+    esac
+done
+
+launch () {
+    echo "Launch $1 $2"
+    cd "$SRC/$1" && gnome-terminal --window-with-profile=KeepOpen -- $2
+}
+
+declare -a INSTALLS=("js/common" "apps/listwebserver" "apps/gui" "apps/capture_probe")
+
+if [ $INSTALL -eq 1 ] ; then
+    echo "INSTALL"
+    ## now loop through the above array
+    for d in "${INSTALLS[@]}"
+    do
+        echo "Installing in $d"
+        launch "$d" "npm install"
+    done
+fi
+
+if [ $RUN_ALL -eq 1 ] ; then
+    echo "RUN_ALL"
+    launch "apps/listwebserver" "npm run dev -- ~/.list/config.yml --dev --live"
+    launch "apps/gui" "npm start"
+    launch "apps/capture_probe" "npm run dev -- config.yml"
+fi
+
