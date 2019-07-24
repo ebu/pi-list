c72851b2d096def7a38ca84575e60109d37b5c51
Pedro Ferreira <pedro@bisect.pt>
2019-07-24 19:38:52 +0000

    Merged PR 145: Ignore VLAN tags
    Ignore VLAN tags
    
    Related work items: #9

--- cpp/libs/net/lib/src/ebu/list/net/ethernet/decoder.cpp
+++ cpp/libs/net/lib/src/ebu/list/net/ethernet/decoder.cpp
@@ -12,10 +12,33 @@ using namespace ebu_list;
 std::tuple<header, oview> ethernet::decode(oview&& l2_packet)
 {
     using ethernet_header_slice = mapped_oview<ethernet::l2_header>;
-    auto[ethernet_header, ethernet_payload] = split(std::move(l2_packet), sizeof(ethernet::l2_header));
-    ethernet_header_slice s(std::move(ethernet_header));
-    header h{ s.value().destination_address, s.value().source_address, static_cast<payload_type>(to_native(s.value().type)) };
-    return { h, ethernet_payload };
+
+    auto copied_packet = oview(l2_packet);
+
+    {
+        auto[ethernet_header, ethernet_payload] = split(std::move(l2_packet), sizeof(ethernet::l2_header));
+        ethernet_header_slice s(std::move(ethernet_header));
+        header h{ s.value().destination_address, s.value().source_address, static_cast<payload_type>(to_native(s.value().type)) };
+
+        if(h.type != payload_type::VLAN_802_1Q)
+        {
+            return { h, ethernet_payload };
+        }
+    }
+
+    {
+        constexpr auto _802_1q_additional_size = 0x04;
+
+        auto[full_ethernet_header, ethernet_payload] = split(std::move(l2_packet), sizeof(ethernet::l2_header) + _802_1q_additional_size);
+        auto full_ethernet_header_copy = oview(full_ethernet_header);
+        auto[ethernet_header, _rest] = split(std::move(full_ethernet_header), sizeof(ethernet::l2_header));
+        ethernet_header_slice s(std::move(ethernet_header));
+        net_uint16_t type;
+        memcpy(reinterpret_cast<void*>(&type), full_ethernet_header_copy.view().data() + 16, sizeof(type));
+        header h{ s.value().destination_address, s.value().source_address, static_cast<payload_type>(to_native(type)) };
+
+        return { h, ethernet_payload };
+    }
 }
 
 std::ostream& ethernet::operator<<(std::ostream& os, const header& h)
