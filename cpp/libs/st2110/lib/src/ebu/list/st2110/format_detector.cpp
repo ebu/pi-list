2717fca1bd684fd9dd85c89216a2492c4dd95440
Pedro Ferreira <pedro@bisect.pt>
2019-09-19 11:43:00 +0000

    Merged PR 233: Avoid using dynamic_cast
    Avoid using dynamic_cast
    
    Related work items: #434

--- cpp/libs/st2110/lib/src/ebu/list/st2110/format_detector.cpp
+++ cpp/libs/st2110/lib/src/ebu/list/st2110/format_detector.cpp
@@ -1,8 +1,8 @@
-#include "ebu/list/st2110/pch.h"
 #include "ebu/list/st2110/format_detector.h"
 #include "ebu/list/st2110/d20/video_format_detector.h"
 #include "ebu/list/st2110/d30/audio_format_detector.h"
 #include "ebu/list/st2110/d40/anc_format_detector.h"
+#include "ebu/list/st2110/pch.h"
 
 using namespace ebu_list;
 using namespace ebu_list::st2110;
@@ -22,26 +22,15 @@ void format_detector::on_data(const rtp::packet& packet)
     std::vector<const detector*> to_remove;
     const detector* one_valid = nullptr;
 
-    for(auto& d : detectors_)
+    for (auto& d : detectors_)
     {
         const auto result = d->handle_data(packet);
         if (result.state == detector::state::invalid)
         {
-            //logger()->debug("Detector marked this stream as not target format");
-
-            if (dynamic_cast<d20::video_format_detector *>(d.get()) != nullptr)
-            {
-                error_codes_list_["video"].push_back(result.error_code);
-            }
-            else if (dynamic_cast<d30::audio_format_detector *>(d.get()) != nullptr)
-            {
-                error_codes_list_["audio"].push_back(result.error_code);
-            }
-            else if (dynamic_cast<d40::anc_format_detector *>(d.get()) != nullptr)
-            {
-                error_codes_list_["anc"].push_back(result.error_code);
-            }
-
+            // logger()->debug("Detector marked this stream as not target
+            // format");
+            const auto kind = d->get_kind();
+            error_codes_list_[kind].push_back(result.error_code);
             to_remove.push_back(d.get());
         }
 
@@ -54,31 +43,33 @@ void format_detector::on_data(const rtp::packet& packet)
 
     if (one_valid)
     {
-        detectors_.erase(remove_if(begin(detectors_), end(detectors_),
-            [&](const auto& x) { return x.get() != one_valid; }),
+        detectors_.erase(
+            remove_if(begin(detectors_), end(detectors_),
+                      [&](const auto& x) { return x.get() != one_valid; }),
             end(detectors_));
     }
     else
     {
         detectors_.erase(remove_if(begin(detectors_), end(detectors_),
-            [&](const auto& x) { return find(begin(to_remove), end(to_remove), x.get()) != end(to_remove); }),
-            end(detectors_));
+                                   [&](const auto& x) {
+                                       return find(begin(to_remove),
+                                                   end(to_remove),
+                                                   x.get()) != end(to_remove);
+                                   }),
+                         end(detectors_));
     }
 
     if (detectors_.empty())
     {
         status_description_.state = detector::state::invalid;
-        status_description_.error_code = "STATUS_CODE_FORMAT_NO_DETECTORS_FOUND";
+        status_description_.error_code =
+            "STATUS_CODE_FORMAT_NO_DETECTORS_FOUND";
     }
 }
 
-void format_detector::on_complete()
-{
-}
+void format_detector::on_complete() {}
 
-void format_detector::on_error(std::exception_ptr)
-{
-}
+void format_detector::on_error(std::exception_ptr) {}
 
 detector::status_description format_detector::status() const noexcept
 {
@@ -87,14 +78,15 @@ detector::status_description format_detector::status() const noexcept
 
 detector::details format_detector::get_details() const
 {
-    if (status_description_.state != detector::state::valid) return std::nullopt;
+    if (status_description_.state != detector::state::valid)
+        return std::nullopt;
 
     assert(detectors_.size() == 1);
     return detectors_[0]->get_details();
 }
 
-std::map<std::string, std::vector<std::string>> format_detector::get_error_codes()
+const std::map<std::string, std::vector<std::string>>&
+format_detector::get_error_codes() const
 {
     return error_codes_list_;
 }
-
