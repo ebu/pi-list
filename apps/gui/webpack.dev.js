d57b4caba4499530b4d7aab92b285ad1576999aa
Ricardo Silva <ricardo@bisect.pt>
2019-10-22 10:05:50 +0000

    Merged PR 249: Improove collaborative translations witth Google Sheets
    Related work items: #451

--- apps/gui/webpack.dev.js
+++ apps/gui/webpack.dev.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const WebpackShellPlugin = require('webpack-shell-plugin');
 
 module.exports = merge(common, {
     mode: 'development',
@@ -11,6 +12,9 @@ module.exports = merge(common, {
             'process.env': {
                 NODE_ENV: JSON.stringify('development'),
             },
+        }),
+        new WebpackShellPlugin({
+            onBuildStart: ['node ./data/translationsGenerator']
         }),
     ],
     devServer: {
--- scripts/build_node.sh
+++ scripts/build_node.sh
@@ -24,14 +24,6 @@ npm install
 echo "Compiling capture probe... done"
 
 echo
-echo "Generating translation..."
-cd $TOP_DIR/apps/gui
-npm install
-cd $TOP_DIR/apps/gui/data/
-node ./translationsGenerator.js
-echo "Generating translation... done"
-
-echo
 echo "Compiling GUI..."
 cd $TOP_DIR/apps/gui/
 npm install && npm run production
