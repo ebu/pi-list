d529cb2e6178c9802e0d42fa036ff4e65c3806bd
Ricardo Silva <ricardo@bisect.pt>
2020-01-17 19:19:29 +0000

    Merged PR 309: Remove unused fields from the Credits source file
    Remove unused fields from the Credits source file
    
    Related work items: #516

--- apps/gui/data/credits/generateCredits.js
+++ apps/gui/data/credits/generateCredits.js
@@ -1,132 +1,64 @@
-const { spawn } = require('child_process');
+const crawler = require('npm-license-crawler');
 const path = require('path');
 const fs = require('fs');
 
-// GUI
+// Modules Path
 const GUI_NM_ROOT_PATH = path.resolve(__dirname, '../../');
-const GUI_MODULES_FILEPATH = path.resolve(__dirname, 'guicredits.json');
-
-// MW
 const MW_NM_ROOT_PATH = path.resolve(__dirname, '../../../listwebserver/');
-const MW_MODULES_FILEPATH = path.resolve(__dirname, 'mwcredits.json');
 
-const STDERROR_FILEPATH = path.resolve(__dirname, 'stderr.log');
+// Credits File
+const CREDITS_FILEPATH = path.resolve(__dirname, 'credits.json');
+const CREDITS_UNKNOWN_FILEPATH = path.resolve(__dirname, 'creditsunknown.json');
+
 const ERROR_FILEPATH = path.resolve(__dirname, 'err.log');
-let ERRORS_COUNT = 0;
 
 /*
  ************  Clear files  ***************
  */
-
-/*
-    LOGS
-*/
-fs.stat(STDERROR_FILEPATH, err => {
-    if (err) {
-        return;
-    }
-
-    fs.unlinkSync(STDERROR_FILEPATH);
-});
-
 fs.stat(ERROR_FILEPATH, err => {
-    if (err) {
-        return;
-    }
-
+    if (err) return;
     fs.unlinkSync(ERROR_FILEPATH);
 });
 
+fs.stat(CREDITS_FILEPATH, err => {
+    if (err) return;
+    fs.unlinkSync(CREDITS_FILEPATH);
+});
+
+fs.stat(CREDITS_UNKNOWN_FILEPATH, err => {
+    if (err) return;
+    fs.unlinkSync(CREDITS_UNKNOWN_FILEPATH);
+});
+
 /*
-    GUI
+    LICENSE DUMP
 */
-fs.stat(GUI_MODULES_FILEPATH, err => {
-    if (err) {
-        return;
+const optionsKnown = {
+    start: [`${GUI_NM_ROOT_PATH}`, `${MW_NM_ROOT_PATH}`],
+    json: `${CREDITS_FILEPATH}`,
+    unknown: false,
+    onlyDirectDependencies: true,
+};
+
+crawler.dumpLicenses(optionsKnown, (error, res) => {
+    if (error) {
+        fs.writeFileSync(ERROR_FILEPATH, error, { flag: 'a' }, err => {
+            if (err) throw err;
+        });
     }
-
-    fs.unlinkSync(GUI_MODULES_FILEPATH);
 });
 
-/*
-    MW
- */
-fs.stat(MW_MODULES_FILEPATH, err => {
-    if (err) {
-        return;
+const optionsUnknown = {
+    start: [`${GUI_NM_ROOT_PATH}`, `${MW_NM_ROOT_PATH}`],
+    json: `${CREDITS_UNKNOWN_FILEPATH}`,
+    unknown: true,
+    onlyDirectDependencies: true,
+};
+
+crawler.dumpLicenses(optionsUnknown, (error, res) => {
+    if (error) {
+        fs.writeFileSync(ERROR_FILEPATH, error, { flag: 'a' }, err => {
+            if (err) throw err;
+        });
     }
-
-    fs.unlinkSync(MW_MODULES_FILEPATH);
-});
-
-/*
-    GUI
-*/
-ERRORS_COUNT = 0;
-const checkGUI = spawn(
-    'npx',
-    ['license-checker', `--start ${GUI_NM_ROOT_PATH}`, '--json', '--production', '--direct', '--unknown'],
-    { cwd: `${GUI_NM_ROOT_PATH}` }
-);
-
-checkGUI.stdout.on('data', data => {
-    fs.writeFileSync(GUI_MODULES_FILEPATH, data, { flag: 'a' }, err => {
-        if (err) throw err;
-    });
-});
-
-checkGUI.on('close', code => {
-    console.log(`Exited with code ${code} and ${ERRORS_COUNT} errors.`);
-});
-
-checkGUI.stderr.on('data', data => {
-    fs.writeFileSync(ERROR_FILEPATH, data, { flag: 'a' }, err => {
-        if (err) throw err;
-    });
-});
-
-checkGUI.on('error', errorFromProcess => {
-    fs.writeFileSync(STDERROR_FILEPATH, errorFromProcess, { flag: 'a' }, err => {
-        if (err) throw err;
-    });
-});
-
-checkGUI.on('close', code => {
-    console.log(`\n Credits for GUI soft finished with code ${code} and ${ERRORS_COUNT} errors.`);
-});
-
-/*
-    MW
-*/
-ERRORS_COUNT = 0;
-const checkMW = spawn(
-    'npx',
-    ['license-checker', `--start ${MW_NM_ROOT_PATH}`, '--json', '--production', '--direct', '--unknown'],
-    { cwd: `${MW_NM_ROOT_PATH}` }
-);
-
-checkMW.stdout.on('data', data => {
-    fs.writeFileSync(MW_MODULES_FILEPATH, data, { flag: 'a' }, err => {
-        if (err) throw err;
-    });
-});
-
-checkMW.on('close', code => {
-    console.log(`Exited with code ${code} and ${ERRORS_COUNT} errors.`);
-});
-
-checkMW.stderr.on('data', data => {
-    fs.writeFileSync(ERROR_FILEPATH, data, { flag: 'a' }, err => {
-        if (err) throw err;
-    });
-});
-
-checkMW.on('error', errorFromProcess => {
-    fs.writeFileSync(STDERROR_FILEPATH, errorFromProcess, { flag: 'a' }, err => {
-        if (err) throw err;
-    });
-});
-
-checkMW.on('close', code => {
-    console.log(`\n Credits for MW soft finished with code ${code} and ${ERRORS_COUNT} errors.`);
 });
