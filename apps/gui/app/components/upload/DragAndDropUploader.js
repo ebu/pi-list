fd8b6ed074432f4ca961f1372374288e289980b2
Pedro Ferreira <pedro@bisect.pt>
2020-05-21 19:31:35 +0000

    Merged PR 344: Update to latest react-dropzone
    Related work items: #473

--- apps/gui/app/components/upload/DragAndDropUploader.js
+++ apps/gui/app/components/upload/DragAndDropUploader.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { useCallback, useReducer, useEffect } from 'react';
+import _ from 'lodash';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import Dropzone from 'react-dropzone';
+import { useDropzone } from 'react-dropzone';
 import Button from '../common/Button';
 import { withNotifications } from '../../utils/notifications/withNotifications';
 
-const PanelTitle = (props) => (
+const PanelTitle = props => (
     <div className="row lst-panel__header lst-truncate">
         <div className="col-xs-6">
             <h2 className="fit-to-div">
@@ -17,110 +18,139 @@ const PanelTitle = (props) => (
     </div>
 );
 
+const actions = {
+    addFile: 'addFile', // payload: { index: number, file: <file as received from DnD>}
+    updatePercentage: 'updatePercentage', // payload: { index: <file index>, percentage: < number >}
+    uploadCompleted: 'uploadCompleted', // payload: { index: <file index>}
+    uploadFailed: 'uploadFailed', // payload: { index: <file index>}
+};
 
-class DragAndDropUploader extends Component {
-    constructor(props) {
-        super(props);
+const initialState = {
+    files: [],
+};
 
-        this.state = {
-            dragActivated: false,
-            files: [],
-            uploadProgress: [],
-            isUploading: false,
-            uploadFailed: false,
-            uploadComplete: false
-        };
+const reducer = (state, action) => {
+    switch (action.type) {
+        case actions.addFile: {
+            const { index, file } = action.payload;
+            const newState = _.cloneDeep(state);
+            newState.files[index] = { file: file, couves: 'batatas' };
 
-        this.onDragEnter = this.onDragEnter.bind(this);
-        this.onDrop = this.onDrop.bind(this);
-        this.uploadFile = this.uploadFile.bind(this);
-    }
+            // props.notificationsContext.updateUpload({ ...newState, files: filesRef.current });
+            return newState;
+        }
 
-    onDragEnter() {
-        this.setState({ dragActivated: true });
-    }
-
-    onDrop(files) {
-        this.setState({ files, dragActivated: false });
-        files.map((f, index) => this.uploadFile(f, index));
-    }
-
-    uploadFile(fileToUpload, index) {
-        this.props.uploadApi(fileToUpload, (uploadPercentage) => {
-            const newStateUploadProgress = this.state.uploadProgress;
-            newStateUploadProgress[index] = uploadPercentage;
-
-            const newState = {
-                uploadProgress: newStateUploadProgress,
+        case actions.updatePercentage: {
+            const { index, percentage } = action.payload;
+            const newState = _.cloneDeep(state);
+            newState.files[index] = {
+                ...newState.files[index],
+                uploadProgress: percentage,
                 isUploading: true,
-                uploadComplete: false
+                uploadComplete: false,
             };
 
-            this.setState(newState,
-                () => this.props.notificationsContext.updateUpload(this.state));
+            // props.notificationsContext.updateUpload({ ...newState, files: filesRef.current });
+            return newState;
+        }
 
-        }).then(() => {
-            const newState = { uploadComplete: true, isUploading: false, uploadFailed: false };
+        case actions.uploadCompleted: {
+            const { index } = action.payload;
+            const newState = _.cloneDeep(state);
+            newState.files[index] = {
+                ...newState.files[index],
+                uploadComplete: true,
+                isUploading: false,
+                uploadFailed: false,
+            };
 
-            this.setState(newState,
-                () => this.props.notificationsContext.updateUpload(this.state));
-        }).catch(() => {
-            const newState = { uploadComplete: false, isUploading: false, uploadFailed: true };
+            // props.notificationsContext.updateUpload({ ...newState, files: filesRef.current });
+            return newState;
+        }
 
-            this.setState(newState,
-                () => this.props.notificationsContext.updateUpload(this.state));
-        });
+        case actions.uploadFailed: {
+            const { index } = action.payload;
+            const newState = _.cloneDeep(state);
+            newState.files[index] = {
+                ...newState.files[index],
+                uploadComplete: false,
+                isUploading: false,
+                uploadFailed: true,
+            };
+
+            // props.notificationsContext.updateUpload({ ...newState, files: filesRef.current });
+            return newState;
+        }
+
+        default:
+            return state;
     }
+};
 
-    render() {
-        const dropzoneClassName = classNames(this.props.className, 'lst-file-uploader2');
+const DragAndDropUploader = props => {
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-        const cardClassNames = classNames(
-            'lst-panel',
-            this.props.className
-        );
+    const onDrop = useCallback(receivedFiles => {
+        receivedFiles.map((fileToUpload, index) => {
+            dispatch({ type: actions.addFile, payload: { index: index, file: fileToUpload } });
 
-        const containerClassNames = classNames(
-            'lst-panel__container',
-            this.props.containerClassName,
-            {
-                'lst-panel--no-padding': this.props.noPadding,
-                'lst-panel--padding': !this.props.noPadding,
-                'lst-panel--no-border': this.props.noBorder,
-                'lst-panel--border': !this.props.noBorder,
-                'lst-panel--full-height': this.props.fullHeight
-            }
-        );
+            props
+                .uploadApi(fileToUpload, uploadPercentage => {
+                    dispatch({
+                        type: actions.updatePercentage,
+                        payload: { index: index, percentage: uploadPercentage },
+                    });
+                })
+                .then(() => {
+                    dispatch({ type: actions.uploadCompleted, payload: { index: index } });
+                })
+                .catch(() => {
+                    dispatch({ type: actions.uploadFailed, payload: { index: index } });
+                });
+        });
+    });
 
-        const rightToolbar = (
-            <div className="row end-xs lst-text-right lst-no-margin">
-                <Button icon="file upload" label={this.props.uploadButtonLabel} />
-            </div>
-        );
+    useEffect(() => {
+        if (state.files.length > 0) {
+            props.notificationsContext.updateUpload({ files: state.files });
+        }
+    }, [state.files]);
 
-        return (
-            <Dropzone
-                className={dropzoneClassName}
-                onDragEnter={() => this.setState({ dragActivated: true })}
-                onDrop={this.onDrop}
-                onDragLeave={() => this.setState({ dragActivated: false })}
-                activeClassName="active"
-            >
-                <div className="lst-file-container">
-                    <div className={cardClassNames}>
-                        <div className={containerClassNames}>
-                            <PanelTitle rightToolbar={rightToolbar} {...this.props} />
-                            <hr />
-                            <div onClick={(e) => e.stopPropagation()}>
-                                {this.props.children}
-                            </div>
-                        </div>
+    const dropzoneClassName = classNames(props.className, 'lst-file-uploader2');
+
+    const cardClassNames = classNames('lst-panel', props.className);
+
+    const containerClassNames = classNames('lst-panel__container', props.containerClassName, {
+        'lst-panel--no-padding': props.noPadding,
+        'lst-panel--padding': !props.noPadding,
+        'lst-panel--no-border': props.noBorder,
+        'lst-panel--border': !props.noBorder,
+        'lst-panel--full-height': props.fullHeight,
+    });
+
+    const rightToolbar = (
+        <div className="row end-xs lst-text-right lst-no-margin">
+            <Button icon="file upload" label={props.uploadButtonLabel} />
+        </div>
+    );
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+    return (
+        <div className="lst-file-container">
+            <div className={cardClassNames}>
+                <div {...getRootProps()}>
+                    <PanelTitle rightToolbar={rightToolbar} {...props} />
+                    <div className={`${containerClassNames} ${dropzoneClassName}`}>
+                        <input {...getInputProps()} />
+                        <hr />
+                        <div onClick={e => e.stopPropagation()}>{props.children}</div>
                     </div>
                 </div>
-            </Dropzone>
-        );
-    }
-}
+            </div>
+        </div>
+    );
+};
 
 DragAndDropUploader.propTypes = {
     className: PropTypes.string,
@@ -132,7 +162,7 @@ DragAndDropUploader.propTypes = {
 DragAndDropUploader.defaultProps = {
     className: '',
     uploadButtonLabel: '',
-    uploadApi: () => { },
+    uploadApi: () => {},
     title: null,
 };
 
