dc8fd1b78dc07befd50e29e0ec3aadb470f0bd60
Fernando Moreira <fernando@bisect.pt>
2020-07-20 17:13:42 +0000

    Merged PR 371: Extract core analysis logic of pre-processor into a library
    Related work items: #605

--- cpp/apps/stream_pre_processor/main.cpp
+++ cpp/apps/stream_pre_processor/main.cpp
@@ -1,16 +1,13 @@
 #include "bisect/bicla.h"
 #include "ebu/list/analysis/constants.h"
-#include "ebu/list/analysis/serialization/pcap.h"
-#include "ebu/list/core/platform/parallel.h"
-#include "ebu/list/pcap/player.h"
-#include "ebu/list/ptp/ptp_offset_calculator.h"
-#include "ebu/list/ptp/udp_filter.h"
-#include "ebu/list/rtp/udp_handler.h"
 #include "ebu/list/version.h"
-#include "stream_listener.h"
+#include "ebu/list/database.h"
+#include "ebu/list/preprocessor/stream_listener.h"
+#include "ebu/list/preprocessor/stream_analyzer.h"
 
 using namespace ebu_list;
 using namespace ebu_list::analysis;
+using namespace ebu_list::ptp;
 using nlohmann::json;
 
 namespace
@@ -41,82 +38,37 @@ namespace
         exit(-1);
     }
 
-    json add_ptp_info(const pcap_info& info, const std::optional<ptp::ptp_offset_calculator::info>& maybe_ptp_info)
+    void save_pcap_info_on_db(db_serializer& db, std::string_view pcap_uuid, const json& info)
     {
-        auto j_fi = pcap_info::to_json(info);
-        if(!maybe_ptp_info) return j_fi;
-
-        const auto& ptp_info = *maybe_ptp_info;
-
-        j_fi["ptp"] = json::object();
-
-        if(ptp_info.is_two_step) j_fi["ptp"]["is_two_step"] = ptp_info.is_two_step.value();
-        if(ptp_info.master_id) j_fi["ptp"]["master_id"] = ptp::v2::to_string(ptp_info.master_id.value());
-        if(ptp_info.grandmaster_id) j_fi["ptp"]["grandmaster_id"] = ptp::v2::to_string(ptp_info.grandmaster_id.value());
-        j_fi["ptp"]["average_offset"] =
-            std::chrono::duration_cast<std::chrono::nanoseconds>(ptp_info.average_offset).count();
-
-        return j_fi;
-    }
-
-    pcap_info make_pcap_info(const path& pcap_file, const std::string& pcap_uuid)
-    {
-        auto info             = pcap_info{};
-        info.id               = pcap_uuid;
-        info.filename         = pcap_file.filename().string();
-        info.analyzer_version = ebu_list::version();
-        return info;
-    }
-
-    void run(const config& config)
-    {
-        auto fi                 = make_pcap_info(config.pcap_file, config.pcap_uuid);
-        auto offset_calculator  = std::make_shared<ptp::ptp_offset_calculator>();
-        const auto mongo_db_url = config.mongo_db_url.value_or(MONGO_DEFAULT_URL);
-
-        auto create_handler = [&](rtp::packet first_packet) {
-            return std::make_unique<stream_listener>(first_packet, fi.id, mongo_db_url);
-        };
-
-        auto udp_handler = std::make_shared<rtp::udp_handler>(create_handler);
-        auto filter      = std::make_shared<ptp::udp_filter>(offset_calculator, udp_handler);
-        auto player      = std::make_unique<pcap::pcap_player>(path(config.pcap_file), filter, on_error_exit);
-
-        const auto start_time = std::chrono::steady_clock::now();
-
-        auto launcher = launch(std::move(player));
-
-        launcher.wait();
-
-        const auto end_time        = std::chrono::steady_clock::now();
-        const auto processing_time = end_time - start_time;
-        const auto processing_time_ms =
-            static_cast<double>(std::chrono::duration_cast<std::chrono::milliseconds>(processing_time).count());
-        logger()->info("Processing time: {:.3f} s", processing_time_ms / 1000.0);
-
-        if(launcher.target().pcap_has_truncated_packets())
-        {
-            fi.truncated = true;
-        }
-
-        const auto ptp_info      = offset_calculator->get_info();
-        fi.offset_from_ptp_clock = ptp_info.has_value() ? ptp_info->average_offset : std::chrono::seconds{0};
-
-        const auto j_fi = add_ptp_info(fi, ptp_info);
-
-        db_serializer db(mongo_db_url);
-
-        if(db.find_one(constants::db::offline, constants::db::collections::pcaps,
-                       nlohmann::json{{"id", config.pcap_uuid}}))
+        if(db.find_one(constants::db::offline, constants::db::collections::pcaps, nlohmann::json{{"id", pcap_uuid}}))
         {
 
-            db.update(constants::db::offline, constants::db::collections::pcaps,
-                      nlohmann::json{{"id", config.pcap_uuid}}, j_fi);
+            db.update(constants::db::offline, constants::db::collections::pcaps, nlohmann::json{{"id", pcap_uuid}},
+                      info);
         }
         else
         {
-            db.insert(constants::db::offline, constants::db::collections::pcaps, j_fi);
+            db.insert(constants::db::offline, constants::db::collections::pcaps, info);
         }
+    }
+
+    void save_stream_info_on_db(db_serializer& db, const json& info)
+    {
+        db.insert(constants::db::offline, constants::db::collections::streams, info);
+    }
+
+    void save_to_db(std::string_view mongo_db_url, json info)
+    {
+        db_serializer db(mongo_db_url);
+        const auto pcap = info["pcap"];
+        const auto pcap_uuid = pcap["id"].get<std::string>();
+        save_pcap_info_on_db(db, pcap_uuid, pcap);
+
+        const auto streams = info["streams"];
+
+        std::for_each(begin(streams), end(streams), [&](const json& stream) {
+          save_stream_info_on_db(db, stream);
+        });
     }
 } // namespace
 
@@ -131,7 +83,8 @@ int main(int argc, char* argv[])
 
     try
     {
-        run(config);
+        const auto info = analyze_stream(config.pcap_file.string(), config.pcap_uuid);
+        save_to_db(config.mongo_db_url.value_or(MONGO_DEFAULT_URL), info);
     }
     catch(std::exception& ex)
     {
---
+++ cpp/libs/preprocessor/unit_tests/ebu/list/preprocessor/test_analyzer.cpp
@@ -0,0 +1,64 @@
+#include "catch2/catch.hpp"
+#include "ebu/list/preprocessor/stream_analyzer.h"
+#include "ebu/list/test_lib/sample_files.h"
+
+SCENARIO("ST2110-X stream identification")
+{
+    GIVEN("An ST2110-20 pcap file")
+    {
+        auto pcap_file = ebu_list::test_lib::sample_file("pcap/st2110/2110-20/2110-20_1080i5994.pcap");
+
+        WHEN("It is analyzed")
+        {
+            const std::string uuid = "62df0243-7154-4d2c-b005-7e11a2b9ea38";
+
+            nlohmann::json analysis = ebu_list::analysis::analyze_stream(
+                pcap_file.string(), uuid
+            );
+
+            THEN("A video stream is found")
+            {
+                REQUIRE(analysis["streams"][0]["media_type"] == "video");
+                // std::cout << analysis.dump(2);
+            }
+        }
+    }
+
+    GIVEN("An ST2110-30 pcap file")
+    {
+        auto pcap_file = ebu_list::test_lib::sample_file("pcap/st2110/2110-30/l24_48000_8ch_0125.pcap");
+
+        WHEN("It is analyzed")
+        {
+            const std::string uuid = "62df0243-7154-4d2c-b005-7e11a2b9ea38";
+
+            nlohmann::json analysis = ebu_list::analysis::analyze_stream(
+                pcap_file.string(), uuid
+            );
+
+            THEN("An audio stream is found")
+            {
+                REQUIRE(analysis["streams"][0]["media_type"] == "audio");
+            }
+        }
+    }
+
+    GIVEN("An ST2110-40 pcap file")
+    {
+        auto pcap_file = ebu_list::test_lib::sample_file("pcap/st2110/2110-40/2110-40_5994i.pcap");
+
+        WHEN("It is analyzed")
+        {
+            const std::string uuid = "62df0243-7154-4d2c-b005-7e11a2b9ea38";
+
+            nlohmann::json analysis = ebu_list::analysis::analyze_stream(
+                pcap_file.string(), uuid
+            );
+
+            THEN("An anc stream is found")
+            {
+                REQUIRE(analysis["streams"][0]["media_type"] == "ancillary_data");
+            }
+        }
+    }
+}
