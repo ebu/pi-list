ebu/pi-list

commit 2d55f0393f70e45239aefa0e11d9890b7bfd8a7a

Merged PR 430: Downsample audio RTP graph
- server: fix "mean" label for influxdb / line graphes
- ui, server: rename audio rtp raw query
- ui, server: downsample audio rtp to 1k using influxdb grouping

Related work items: #686

apps/listwebserver/managers/influx-db.js
@@ -30,6 +30,10 @@ class InfluxDbManager {
         return `time >= ${startTime}ns and time <= ${endTime}ns`;
     }
 
+    timeGroupFilter(startTime, endTime, groupTime) {
+        return `time >= ${startTime}ns and time <= ${endTime}ns group by time(${groupTime}ns)`;
+    }
+
     sendQueryAndFormatResults(query) {
         return this.influx.query(query).then(data =>
             data.map(item => ({
@@ -128,7 +132,7 @@ class InfluxDbManager {
 
     getDeltaPacketTimeVsRtpTimeMinMax(pcapID, streamID) {
         const query = `
-            select MAX("delta_packet_time_vs_rtp_time_ns") as "max", MIN("delta_packet_time_vs_rtp_time_ns") as "min", MEAN("delta_packet_time_vs_rtp_time_ns") as "avg"
+            select MAX("delta_packet_time_vs_rtp_time_ns") as "max", MIN("delta_packet_time_vs_rtp_time_ns") as "min", MEAN("delta_packet_time_vs_rtp_time_ns") as "mean"
             ${this.fromPcapIdWhereStreamIs(pcapID, streamID)}`;
 
         log.info(
@@ -153,7 +157,7 @@ class InfluxDbManager {
 
     getDeltaRtpVsNtTicksMinMax(pcapID, streamID) {
         const query = `
-            select MAX("delta_rtp_vs_NTs") as "max", MIN("delta_rtp_vs_NTs") as "min", MEAN("delta_rtp_vs_NTs") as "avg"
+            select MAX("delta_rtp_vs_NTs") as "max", MIN("delta_rtp_vs_NTs") as "min", MEAN("delta_rtp_vs_NTs") as "mean"
             ${this.fromPcapIdWhereStreamIs(pcapID, streamID)}`;
 
         log.info(`Get DeltaRtpVsNtTicksMinMax for the stream ${streamID} in the pcap ${pcapID}. Query: \n ${query}`);
@@ -176,7 +180,7 @@ class InfluxDbManager {
 
     getDeltaToPreviousRtpTsMinMax(pcapID, streamID, startTime, endTime) {
         const query = `
-            select MAX("delta_previous_rtp_ts") as "max", MIN("delta_previous_rtp_ts") as "min", MEAN("delta_previous_rtp_ts") as "avg"
+            select MAX("delta_previous_rtp_ts") as "max", MIN("delta_previous_rtp_ts") as "min", MEAN("delta_previous_rtp_ts") as "mean"
             ${this.fromPcapIdWhereStreamIs(pcapID, streamID)}
         `;
 
@@ -197,11 +201,23 @@ class InfluxDbManager {
         return this.sendQueryAndFormatResults(query);
     }
 
-    getAudioPktTsVsRtpTs(pcapID, streamID, startTime, endTime) {
+    getAudioPktTsVsRtpTsRaw(pcapID, streamID, startTime, endTime) {
         const query = `
             select
             "audio-pkt-vs-rtp" as "value"
             ${this.fromPcapIdWhereStreamIs(pcapID, streamID)} and ${this.timeFilter(startTime, endTime)}
+        `;
+
+        log.info(`Get RTP-TS vs PKT-TS for the stream ${streamID} in the pcap ${pcapID}. Query: \n ${query}`);
+
+        return this.sendQueryAndFormatResults(query);
+    }
+
+    getAudioPktTsVsRtpTsGrouped(pcapID, streamID, startTime, endTime, groupTime) {
+        const query = `
+            select
+            max("audio-pkt-vs-rtp") as "max", mean("audio-pkt-vs-rtp") as "mean", min("audio-pkt-vs-rtp") as "min"
+            ${this.fromPcapIdWhereStreamIs(pcapID, streamID)} and ${this.timeGroupFilter(startTime, endTime, groupTime)}
         `;
 
         log.info(`Get RTP-TS vs PKT-TS for the stream ${streamID} in the pcap ${pcapID}. Query: \n ${query}`);

apps/gui/app/components/graphs/index.js
@@ -4,6 +4,6 @@ import { histogramAsPercentages } from './utils';
 
 import './index.scss';
 
-export default { Line: LineGraphHOC, Histogram: HistogramHOC };
+export default { Line: LineGraphHOC, Histogram: HistogramHOC, lineSize: 1000 };
 
 export { histogramAsPercentages };

apps/gui/app/containers/AudioPage/RTP.js
@@ -15,11 +15,12 @@ const RTP = props => {
                         xTitleTag="media_information.timeline"
                         yTitleTag="media_information.delay"
                         asyncGetter={() =>
-                            api.getAudioPktTsVsRtpTs(
+                            api.getAudioPktTsVsRtpTsGrouped(
                                 props.pcapID,
                                 props.streamID,
                                 first_packet_ts,
-                                last_packet_ts)
+                                last_packet_ts,
+                                Math.round((last_packet_ts - first_packet_ts) / Graphs.lineSize))
                         }
                     />
                 </div>

apps/gui/app/utils/AVSync.js
@@ -22,7 +22,7 @@ export const getAudioCursor = (mp3Duration, mp3CurrentTime, audioInfo) => {
     /* compute RTP ts from pkt ts - deltaPktVsRtp */
     const margin_sec = audioInfo.packet_time / msPerSec / 2;
 
-    return api.getAudioPktTsVsRtpTs(
+    return api.getAudioPktTsVsRtpTsRaw(
         audioInfo.pcap,
         audioInfo.stream,
         (absTime - margin_sec) * nsPerSec,

apps/gui/app/utils/api.js
@@ -184,8 +184,10 @@ const api = {
         request.get(`pcap/${pcapID}/stream/${streamID}/analytics/DeltaRtpVsNt?from=${fromNs}&to=${toNs}`),
     getPacketsPerFrame: (pcapID, streamID, fromNs, toNs) =>
         request.get(`pcap/${pcapID}/stream/${streamID}/analytics/packetsPerFrame?from=${fromNs}&to=${toNs}`),
-    getAudioPktTsVsRtpTs: (pcapID, streamID, fromNs, toNs) =>
-        request.get(`pcap/${pcapID}/stream/${streamID}/analytics/AudioPktTsVsRtpTs?from=${fromNs}&to=${toNs}`),
+    getAudioPktTsVsRtpTsRaw: (pcapID, streamID, fromNs, toNs) =>
+        request.get(`pcap/${pcapID}/stream/${streamID}/analytics/AudioPktTsVsRtpTsRaw?from=${fromNs}&to=${toNs}`),
+    getAudioPktTsVsRtpTsGrouped: (pcapID, streamID, fromNs, toNs, groupNs) =>
+        request.get(`pcap/${pcapID}/stream/${streamID}/analytics/AudioPktTsVsRtpTsGrouped?from=${fromNs}&to=${toNs}&group=${groupNs}`),
     getAudioTimeStampedDelayFactor: (pcapID, streamID, fromNs, toNs, toleranceUs, tsdfmaxUs) =>
         request.get(
             `pcap/${pcapID}/stream/${streamID}/analytics/AudioTimeStampedDelayFactor?from=${fromNs}&to=${toNs}&tolerance=${toleranceUs}&tsdfmax=${tsdfmaxUs}`

apps/listwebserver/api/pcap.js
@@ -328,11 +328,23 @@ router.get('/:pcapID/stream/:streamID/analytics/Vrx/histogram', (req, res) => {
 });
 
 /* Audio Delays */
-router.get('/:pcapID/stream/:streamID/analytics/AudioPktTsVsRtpTs', (req, res) => {
+router.get('/:pcapID/stream/:streamID/analytics/AudioPktTsVsRtpTsRaw', (req, res) => {
     const { pcapID, streamID } = req.params;
     const { from, to } = req.query;
 
-    chartData = influxDbManager.getAudioPktTsVsRtpTs(pcapID, streamID, from, to);
+    chartData = influxDbManager.getAudioPktTsVsRtpTsRaw(pcapID, streamID, from, to);
+    chartData
+        .then((data) => {
+            res.json(data);
+        })
+        .catch(() => res.status(HTTP_STATUS_CODE.CLIENT_ERROR.NOT_FOUND).send(API_ERRORS.RESOURCE_NOT_FOUND));
+});
+
+router.get('/:pcapID/stream/:streamID/analytics/AudioPktTsVsRtpTsGrouped', (req, res) => {
+    const { pcapID, streamID } = req.params;
+    const { from, to, group } = req.query;
+
+    chartData = influxDbManager.getAudioPktTsVsRtpTsGrouped(pcapID, streamID, from, to, group);
     chartData
         .then((data) => {
             res.json(data);

commit 96583d7215517cfa5e56736049db083a47dd8d21

Merged PR 439: server: fix avg measurement inconsistency caused by 2d55f039
server: fix avg measurement inconsistency caused by 2d55f039

Video analysis page was broken because of that.

Related work items: #750

apps/listwebserver/managers/influx-db.js
@@ -132,7 +132,7 @@ class InfluxDbManager {
 
     getDeltaPacketTimeVsRtpTimeMinMax(pcapID, streamID) {
         const query = `
-            select MAX("delta_packet_time_vs_rtp_time_ns") as "max", MIN("delta_packet_time_vs_rtp_time_ns") as "min", MEAN("delta_packet_time_vs_rtp_time_ns") as "mean"
+            select max("delta_packet_time_vs_rtp_time_ns") as "max", min("delta_packet_time_vs_rtp_time_ns") as "min", mean("delta_packet_time_vs_rtp_time_ns") as "avg"
             ${this.fromPcapIdWhereStreamIs(pcapID, streamID)}`;
 
         log.info(
@@ -157,7 +157,7 @@ class InfluxDbManager {
 
     getDeltaRtpVsNtTicksMinMax(pcapID, streamID) {
         const query = `
-            select MAX("delta_rtp_vs_NTs") as "max", MIN("delta_rtp_vs_NTs") as "min", MEAN("delta_rtp_vs_NTs") as "mean"
+            select max("delta_rtp_vs_NTs") as "max", min("delta_rtp_vs_NTs") as "min", mean("delta_rtp_vs_NTs") as "avg"
             ${this.fromPcapIdWhereStreamIs(pcapID, streamID)}`;
 
         log.info(`Get DeltaRtpVsNtTicksMinMax for the stream ${streamID} in the pcap ${pcapID}. Query: \n ${query}`);
@@ -180,7 +180,7 @@ class InfluxDbManager {
 
     getDeltaToPreviousRtpTsMinMax(pcapID, streamID, startTime, endTime) {
         const query = `
-            select MAX("delta_previous_rtp_ts") as "max", MIN("delta_previous_rtp_ts") as "min", MEAN("delta_previous_rtp_ts") as "mean"
+            select max("delta_previous_rtp_ts") as "max", min("delta_previous_rtp_ts") as "min", mean("delta_previous_rtp_ts") as "avg"
             ${this.fromPcapIdWhereStreamIs(pcapID, streamID)}
         `;
 
@@ -216,7 +216,7 @@ class InfluxDbManager {
     getAudioPktTsVsRtpTsGrouped(pcapID, streamID, startTime, endTime, groupTime) {
         const query = `
             select
-            max("audio-pkt-vs-rtp") as "max", mean("audio-pkt-vs-rtp") as "mean", min("audio-pkt-vs-rtp") as "min"
+            max("audio-pkt-vs-rtp") as "max", min("audio-pkt-vs-rtp") as "min", mean("audio-pkt-vs-rtp") as "avg"
             ${this.fromPcapIdWhereStreamIs(pcapID, streamID)} and ${this.timeGroupFilter(startTime, endTime, groupTime)}
         `;
 