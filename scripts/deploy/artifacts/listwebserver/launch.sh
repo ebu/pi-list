f93f2a743a796c607c8c6f02f04c97e98b3f7313
Pedro Ferreira <pedro@bisect.pt>
2021-05-14 19:40:32 +0000

    Merged PR 460: Fix typo
    Related work items: #785

--- scripts/deploy/artifacts/listwebserver/launch.sh
+++ scripts/deploy/artifacts/listwebserver/launch.sh
@@ -6,4 +6,4 @@ serve -s /app/gui -p 8080 &
 npm start -- config.yml --dev &
 
 # Relaunch if it crashes
-while true; do ./bin/stream_pre_processor amqp://rabbitmq:5672 ; done
+while true; do ../bin/stream_pre_processor amqp://rabbitmq:5672 ; done
