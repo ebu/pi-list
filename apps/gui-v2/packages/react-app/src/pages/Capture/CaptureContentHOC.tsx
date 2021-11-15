20eb2fdaab308d2121a1678ce566aec95f31d3f2
Patrick Keroulas <patrick.keroulas@radio-canada.ca>
2021-11-15 16:56:59 -0500

    ui: show capture help

--- apps/gui-v2/packages/react-app/src/pages/Capture/CaptureContentHOC.tsx
+++ apps/gui-v2/packages/react-app/src/pages/Capture/CaptureContentHOC.tsx
@@ -26,7 +26,25 @@ function CaptureContentHOC() {
                         onTileDoubleClick={onTileDoubleClick}
                     />
                 }
-                informationSidebarContent={{ usermail: userInfo?.username }}
+                informationSidebarContent={ {
+                        usermail: userInfo?.username,
+                        content: (
+                            <div style={{ height: '100vh', overflow: 'auto' }}>
+                                <div className="sb-information-sidebar-content">
+                                    <div className="capture-description-container">
+                                        <span
+                                        className="capture-description-title">Pcap Capture Help</span>
+                                        <span className="capture-description-text">
+- Select one or more <i>Live Sources</i> (streams) from the table. <br/>
+- Set the capture <i>Name</i> param and file <i>Duration</i>. <br/>
+- If multiple sources are selected, choose <i>Mode</i> between <i>Parallel</i> (simultaenous) or <i>Sequential</i>. <br/>
+- Press <i>Capture</i> to perform PCAP capture and analysis.
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                } }
                 logout={() => history.push('/logout')}
             />
         </>
