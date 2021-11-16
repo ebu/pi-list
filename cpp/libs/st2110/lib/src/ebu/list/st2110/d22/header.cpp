b4cb827b2a7bf2552fb6f9811810a7dcf9bc9670
mariana <mariana@bisect.pt>
2021-11-16 15:36:43 +0000

    Unit test for payload header of st2110-22

--- cpp/libs/st2110/lib/src/ebu/list/st2110/d22/header.cpp
+++ cpp/libs/st2110/lib/src/ebu/list/st2110/d22/header.cpp
@@ -4,7 +4,7 @@ using namespace ebu_list::st2110::d22;
 
 header::header(cbyte_span data)
 {
-    assert(data.size() >= sizeof(payload_header_));
+    assert(static_cast<size_t>(data.size()) >= sizeof(payload_header_));
     memcpy(payload_header_.data(), data.data(), sizeof(payload_header_));
 }
 
@@ -30,40 +30,39 @@ uint16_t header::get_sep_counter()
     return v;
 }
 
-
 uint8_t header::get_f_counter()
 {
     uint8_t v = 0;
     v += (static_cast<uint8_t>(payload_header_[1]) >> 6) & 0x03;
-    v += static_cast<uint8_t>(payload_header_[0]) & 0x0F;
+    v += (static_cast<uint8_t>(payload_header_[0]) & 0x07) << 2;
     return v;
 }
 
 uint8_t header::get_interlaced()
 {
     uint8_t v = 0;
-    v += (3 << static_cast<uint8_t>(payload_header_[0]) >> 3) & 0x03;
+    v += (static_cast<uint8_t>(payload_header_[0]) & 0x18) >> 3;
     return v;
 }
 
 uint8_t header::get_last()
 {
     uint8_t v = 0;
-    v += (2 << static_cast<uint8_t>(payload_header_[0]) >> 5) & 0x01;
+    v += (static_cast<uint8_t>(payload_header_[0]) & 0x20) >> 5;
     return v;
 }
 
 uint8_t header::get_packetization_mode()
 {
     uint8_t v = 0;
-    v += (1 << static_cast<uint8_t>(payload_header_[0]) >> 6) & 0x01;
+    v += (static_cast<uint8_t>(payload_header_[0]) & 0x40) >> 6;
     return v;
 }
 
 uint8_t header::get_transmission_mode()
 {
     uint8_t v = 0;
-    v += (static_cast<uint8_t>(payload_header_[0]) >> 7) & 0x01;
+    v += (static_cast<uint8_t>(payload_header_[0]) & 0x80) >> 7;
     return v;
 }
 //////////////////////////////////////////////////////////////////////
---
+++ cpp/libs/st2110/unit_tests/ebu/list/st2110/d22/test_header.cpp
@@ -0,0 +1,37 @@
+#include "catch2/catch.hpp"
+#include "ebu/list/core/types.h"
+#include "ebu/list/st2110/d22/header.h"
+
+using namespace ebu_list;
+using namespace ebu_list::st2110::d22;
+
+//------------------------------------------------------------------------------
+SCENARIO("st2110-22 payload header")
+{
+    GIVEN("a buffer with 4 bytes")
+    {
+        const auto buffer = to_byte_array(0x80, 0xC0, 0, 0x4B);
+        // byte buffer[4] = {byte(0), byte(0), byte(0), byte(0x4B)};
+
+        WHEN("we check the fields of the payload header")
+        {
+            auto payload_header = header(buffer);
+
+            auto p_counter         = payload_header.get_p_counter();
+            auto sep_counter       = payload_header.get_sep_counter();
+            auto f_counter          = payload_header.get_f_counter();
+            auto interlaced         = payload_header.get_interlaced();
+            auto last               = payload_header.get_last();
+            auto packetization_mode = payload_header.get_packetization_mode();
+            auto transmission_mode  = payload_header.get_transmission_mode();
+
+            THEN("p counter must be 0x4B") { REQUIRE(p_counter == 0x4B); }
+            THEN("sep counter must be 0") { REQUIRE(sep_counter == 0); }
+            THEN("f counter must be 0") { REQUIRE(f_counter == 3); }
+            THEN("interlaced must be 0") { REQUIRE(interlaced == 0); }
+            THEN("last must be 0") { REQUIRE(last == 0); }
+            THEN("packetization_mode must be 0") { REQUIRE(packetization_mode == 0); }
+            THEN("transmission_mode must be 0") { REQUIRE(transmission_mode == 1); }
+        }
+    }
+}
