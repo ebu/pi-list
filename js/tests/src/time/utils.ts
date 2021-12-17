43b26abe3d8bf00e3441a0c926dfb4cb7b45b4bc
Nelson Silva <nelson@bisect.pt>
2021-12-17 09:00:19 +0000

    Time tests using uploadLocal endpoint

--- js/tests/src/time/utils.ts
+++ js/tests/src/time/utils.ts
@@ -9,10 +9,50 @@ import { testUtils } from '@bisect/bisect-core-ts-be';
 import { addTest as add, requirements } from '../repo';
 import path from 'path';
 import { loginOrRegister } from '../basic/auth';
-import { doUpload } from '../basic/pcap';
+// import { doUpload } from '../basic/pcap';
 import { performance } from 'perf_hooks';
 var diff = require('deep-diff');
 const tmp = require('temporary');
+
+const doUploadTimeTests = async (
+    list: LIST,
+    name: string,
+    stream: fs.ReadStream,
+    pcapFile: string,
+    callback: types.UploadProgressCallback
+): Promise<string> =>
+    new Promise(async (resolve, reject) => {
+        try {
+            const wsClient = list.wsClient;
+            if (wsClient === undefined) {
+                reject(new Error('WebSocket client not connected'));
+                return;
+            }
+
+            const pcapId = uuid();
+            const timeoutMs = 300000; // It may be necessary to increase timeout due to the size of the pcap file
+
+            const uploadAwaiter = list.pcap.makeUploadAwaiter(pcapId, timeoutMs);
+            await list.pcap.uploadLocal(name, pcapFile, pcapId);
+            const uploadResult = await uploadAwaiter;
+
+            if (!uploadResult) {
+                reject(new Error('Pcap processing undefined'));
+                return;
+            }
+
+            console.log(`Awaiter: ${JSON.stringify(uploadResult)}`);
+
+            if (pcapId !== uploadResult.id) {
+                reject(new Error('Pcap id undefined'));
+                return;
+            }
+
+            resolve(pcapId);
+        } catch (err) {
+            reject(err);
+        }
+    });
 
 export const runUploadTimeTest = async (name: string, c: testUtils.ITestContext) => {
     const list = new LIST(c.settings.address);
@@ -30,7 +70,7 @@ export const runUploadTimeTest = async (name: string, c: testUtils.ITestContext)
 
         const begin = performance.now();
         const callback = (info: types.IUploadProgressInfo) => console.log(`percentage: ${info.percentage}`);
-        const pcapId = await doUpload(list, name, stream, callback);
+        const pcapId = await doUploadTimeTests(list, name, stream, pcapFile, callback);
         const end = performance.now();
         const duration = (end - begin) / 1000;
         const logTime = `Duration time for pcap ${name}: ${duration} seconds \r\n`;
