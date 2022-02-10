ceba5362cc32e5691bd58915e8fcee98bccc70f3
Pedro Ferreira <pedro@bisect.pt>
2022-02-10 11:12:32 +0000

    Adjust parameters

--- apps/gui-v2/packages/react-app/src/utils/useLoginWithToken.tsx
+++ apps/gui-v2/packages/react-app/src/utils/useLoginWithToken.tsx
@@ -3,11 +3,11 @@ import queryString from 'query-string';
 import { useHistory, useLocation } from 'react-router-dom';
 import list from 'utils/api';
 
-const getQueryParamAsString = (location: null | string | string[], or: string): string => {
+const getQueryParamAsString = (location: null | string | (string | null)[], or: string): string => {
     if (!location) return or;
 
     if (Array.isArray(location)) {
-        return location[0];
+        return location[0] || or;
     }
     return location;
 };
