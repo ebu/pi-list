af45c6c852e851fe3ef750186e9fb6fbab44e3b9
diego <diego@bisect.pt>
2022-04-18 15:14:44 +0100

    Fixed redirect

--- apps/gui-v2/packages/react-app/src/routes/index.tsx
+++ apps/gui-v2/packages/react-app/src/routes/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import routeInfo, { IRouteInfo } from './routeInfo';
 import routeNames from './routeNames';
 
 const routeFor = (e: IRouteInfo, index: number): JSX.Element => {
-    return <Route key={index} path={e.path} element={e.component}/>;
+    return <Route key={index} path={e.path} element={e.component} />;
 };
 
 export default (
     <Routes>
-        <Route path={routeNames.HOME}>
+        <Route path={routeNames.HOME} element={<Navigate to="/pcaps" />} />
         {routeInfo.map((e, index) => routeFor(e, index))}
-        </Route>
         <Route element={<h1>ERROR</h1>} />
     </Routes>
 );
